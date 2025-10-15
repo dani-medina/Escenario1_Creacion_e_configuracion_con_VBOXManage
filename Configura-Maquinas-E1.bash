07c4edcfc1b80af4d431a0848e065fbcd2da31b7
Daniel Medina Méndez <danielmedinamendez@gmail.com>
2025-10-15 21:03:04 +0200

    Engadida a comprobación de que a máquina xa foi configurada previamente

--- Configura-Maquinas-E1.bash
+++ Configura-Maquinas-E1.bash
@@ -25,9 +25,7 @@ FICHEROMAQUINAS=/tmp/ListaMaquinas.txt
 
 #Comezamos a configurar os equipos Windows
 POWERSHELL="C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" 
-for LINEA in $(cat ${FICHEROMAQUINAS} | grep Windows)
-#for LINEA in $(cat ${FICHEROMAQUINAS} | grep NADA)
-do
+for LINEA in $(cat ${FICHEROMAQUINAS} | grep Windows); do
 	MAQUINA=$(echo ${LINEA} | cut -d : -f 1)
 	IP=$(echo ${LINEA} | cut -d : -f 3)
 	USUARIO=$(echo ${LINEA} | cut -d : -f 4)
@@ -51,25 +49,35 @@ do
 		sleep 5		
 	done
 	# Renomeado do equipo
-	vboxmanage guestcontrol ${MAQUINA} \
+	NOME=$(vboxmanage guestcontrol ${MAQUINA} \
 		--username administrador \
 		--password ${CONTRASINAL} \
 		run  ${POWERSHELL} \
-		-- -command Rename-Computer ${MAQUINA} 
-	# IP, MASCARA e GATEWAY
-	if [ ! "${IP}" = "DHCP" ]; then
-		vboxmanage guestcontrol ${MAQUINA} \
-			--username administrador \
-			--password ${CONTRASINAL} \
-			run  ${POWERSHELL} \
-			-- -command New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress ${IP} -PrefixLength ${MASCARAINT} -DefaultGateway ${GWINT}
+		-- -command Invoke-Expression -Command 'hostname' |  tr -d '\r\n')
 
-		# DNS
+ 	if [ "${NOME}" = "${MAQUINA}" ]; then 
+		echo A máquina xa foi configurada previamente.
+	else 
 		vboxmanage guestcontrol ${MAQUINA} \
 			--username administrador \
 			--password ${CONTRASINAL} \
 			run  ${POWERSHELL} \
-			-- -command Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses ${DNSINT}
+			-- -command Rename-Computer ${MAQUINA} 
+		# IP, MASCARA e GATEWAY
+		if [ ! "${IP}" = "DHCP" ]; then
+			vboxmanage guestcontrol ${MAQUINA} \
+				--username administrador \
+				--password ${CONTRASINAL} \
+				run  ${POWERSHELL} \
+				-- -command New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress ${IP} -PrefixLength ${MASCARAINT} -DefaultGateway ${GWINT}
+
+			# DNS
+			vboxmanage guestcontrol ${MAQUINA} \
+				--username administrador \
+				--password ${CONTRASINAL} \
+				run  ${POWERSHELL} \
+				-- -command Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses ${DNSINT}
+		fi
 	fi
 	
 	# Apagado da máquina
@@ -83,9 +91,7 @@ done
 
 # Configura Equipos Ubuntu
 
-for LINEA in $(cat ${FICHEROMAQUINAS} | grep Ubuntu)
-#for LINEA in $(cat ${FICHEROMAQUINAS} | grep NADA)
-do
+for LINEA in $(cat ${FICHEROMAQUINAS} | grep Ubuntu); do
 	MAQUINA=$(echo ${LINEA} | cut -d : -f 1)
 	IP=$(echo ${LINEA} | cut -d : -f 3)
 	USUARIO=$(echo ${LINEA} | cut -d : -f 4)
@@ -107,47 +113,56 @@ do
 		sleep 5s
 		
 	done
-	sed "s/CAMBIAME/${MAQUINA}/g" files/hosts > /tmp/hosts-${MAQUINA}
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
+	NOME=$(vboxmanage guestcontrol ${MAQUINA} \
+		--username administrador \
 		--password ${CONTRASINAL} \
-		copyto /tmp/hosts-${MAQUINA} /etc/hosts
+		run  --exe "/bin/hostname" |  tr -d '\r\n')
 
-	
-	sed "s/CAMBIAME/${MAQUINA}/g" files/hostname > /tmp/hostname-${MAQUINA}
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		copyto /tmp/hostname-${MAQUINA} /etc/hostname
-
-
-	if [ ! "${IP}" = "DHCP" ]; then
-		
-		NOMENMCLI=$(vboxmanage guestcontrol ${MAQUINA} \
+ 	if [ "${NOME}" = "${MAQUINA}" ]; then 
+		echo A máquina xa foi configurada previamente.
+	else 
+		sed "s/CAMBIAME/${MAQUINA}/g" files/hosts > /tmp/hosts-${MAQUINA}
+		vboxmanage guestcontrol ${MAQUINA} \
 			--username ${USUARIO} \
 			--password ${CONTRASINAL} \
-			run --exe "/usr/bin/nmcli" -- -t -f NAME,TYPE con show --active | grep ethernet | cut -f 1 -d : )
-		if [ ! -z ${NOMENMCLI} ]; then
-			vboxmanage guestcontrol ${MAQUINA} \
+			copyto /tmp/hosts-${MAQUINA} /etc/hosts
+
+		
+		sed "s/CAMBIAME/${MAQUINA}/g" files/hostname > /tmp/hostname-${MAQUINA}
+		vboxmanage guestcontrol ${MAQUINA} \
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/hostname-${MAQUINA} /etc/hostname
+
+
+		if [ ! "${IP}" = "DHCP" ]; then
+			
+			NOMENMCLI=$(vboxmanage guestcontrol ${MAQUINA} \
 				--username ${USUARIO} \
 				--password ${CONTRASINAL} \
-				run --exe "/usr/bin/nmcli" -- con mod ${NOMENMCLI} ipv4.addresses ${IP}/${MASCARAINT}
-			vboxmanage guestcontrol ${MAQUINA} \
-				--username ${USUARIO} \
-				--password ${CONTRASINAL} \
-				run --exe "/usr/bin/nmcli" -- con mod ${NOMENMCLI} ipv4.gateway ${GWINT}
-			vboxmanage guestcontrol ${MAQUINA} \
-				--username ${USUARIO} \
-				--password ${CONTRASINAL} \
-				run --exe "/usr/bin/nmcli" -- con mod ${NOMENMCLI} ipv4.dns ${DNSINT}	
-			vboxmanage guestcontrol ${MAQUINA} \
-				--username ${USUARIO} \
-				--password ${CONTRASINAL} \
-				run --exe "/usr/bin/nmcli" -- con mod ${NOMENMCLI} ipv4.method manual
-			vboxmanage guestcontrol ${MAQUINA} \
-				--username ${USUARIO} \
-				--password ${CONTRASINAL} \
-				run --exe "/usr/bin/nmcli" -- con up ${NOMENMCLI}		
+				run --exe "/usr/bin/nmcli" -- -t -f NAME,TYPE con show --active | grep ethernet | cut -f 1 -d : )
+			if [ ! -z ${NOMENMCLI} ]; then
+				vboxmanage guestcontrol ${MAQUINA} \
+					--username ${USUARIO} \
+					--password ${CONTRASINAL} \
+					run --exe "/usr/bin/nmcli" -- con mod ${NOMENMCLI} ipv4.addresses ${IP}/${MASCARAINT}
+				vboxmanage guestcontrol ${MAQUINA} \
+					--username ${USUARIO} \
+					--password ${CONTRASINAL} \
+					run --exe "/usr/bin/nmcli" -- con mod ${NOMENMCLI} ipv4.gateway ${GWINT}
+				vboxmanage guestcontrol ${MAQUINA} \
+					--username ${USUARIO} \
+					--password ${CONTRASINAL} \
+					run --exe "/usr/bin/nmcli" -- con mod ${NOMENMCLI} ipv4.dns ${DNSINT}	
+				vboxmanage guestcontrol ${MAQUINA} \
+					--username ${USUARIO} \
+					--password ${CONTRASINAL} \
+					run --exe "/usr/bin/nmcli" -- con mod ${NOMENMCLI} ipv4.method manual
+				vboxmanage guestcontrol ${MAQUINA} \
+					--username ${USUARIO} \
+					--password ${CONTRASINAL} \
+					run --exe "/usr/bin/nmcli" -- con up ${NOMENMCLI}		
+			fi
 		fi
 	fi		
 	
@@ -158,9 +173,7 @@ done
 
 #Configuramos equipos Debian Server
 
-for LINEA in $(cat ${FICHEROMAQUINAS} | grep Debian)
-#for LINEA in $(cat ${FICHEROMAQUINAS} | grep NADA)
-do
+for LINEA in $(cat ${FICHEROMAQUINAS} | grep Debian); do
 	MAQUINA=$(echo ${LINEA} | cut -d : -f 1)
 	IP=$(echo ${LINEA} | cut -d : -f 3)
 	USUARIO=$(echo ${LINEA} | cut -d : -f 4)
@@ -182,30 +195,39 @@ do
 		sleep 5s
 			
 	done
-	sed "s/CAMBIAME/${MAQUINA}/g" files/hosts > /tmp/hosts-${MAQUINA}
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
+	NOME=$(vboxmanage guestcontrol ${MAQUINA} \
+		--username administrador \
 		--password ${CONTRASINAL} \
-		copyto /tmp/hosts-${MAQUINA} /etc/hosts
+		run  --exe "/bin/hostname" |  tr -d '\r\n')
 
-	
-	sed "s/CAMBIAME/${MAQUINA}/g" files/hostname > /tmp/hostname-${MAQUINA}
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		copyto /tmp/hostname-${MAQUINA} /etc/hostname
-	if [ ! "${IP}" = "DHCP" ]; then
-		sed "s/CAMBIAME/${IP}/g" files/interfaces-INT > /tmp/interfaces-${MAQUINA}
+ 	if [ "${NOME}" = "${MAQUINA}" ]; then 
+		echo A máquina xa foi configurada previamente.
+	else 
+		sed "s/CAMBIAME/${MAQUINA}/g" files/hosts > /tmp/hosts-${MAQUINA}
 		vboxmanage guestcontrol ${MAQUINA} \
-			--username root \
-			--password renaido \
-			copyto /tmp/interfaces-${MAQUINA} /etc/network/interfaces
-		sed "s/CAMBIAME/${DNSINT}/g" files/resolv.conf-INT > /tmp/resolv.conf-${MAQUINA}
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/hosts-${MAQUINA} /etc/hosts
+
+		
+		sed "s/CAMBIAME/${MAQUINA}/g" files/hostname > /tmp/hostname-${MAQUINA}
 		vboxmanage guestcontrol ${MAQUINA} \
-			--username root \
-			--password renaido \
-			copyto /tmp/resolv.conf-${MAQUINA} /etc/resolv.conf
-	fi		
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/hostname-${MAQUINA} /etc/hostname
+		if [ ! "${IP}" = "DHCP" ]; then
+			sed "s/CAMBIAME/${IP}/g" files/interfaces-INT > /tmp/interfaces-${MAQUINA}
+			vboxmanage guestcontrol ${MAQUINA} \
+				--username root \
+				--password renaido \
+				copyto /tmp/interfaces-${MAQUINA} /etc/network/interfaces
+			sed "s/CAMBIAME/${DNSINT}/g" files/resolv.conf-INT > /tmp/resolv.conf-${MAQUINA}
+			vboxmanage guestcontrol ${MAQUINA} \
+				--username root \
+				--password renaido \
+				copyto /tmp/resolv.conf-${MAQUINA} /etc/resolv.conf
+		fi
+	fi	
 	
 	
 	vboxmanage controlvm ${MAQUINA} acpipowerbutton
@@ -237,72 +259,74 @@ if [ "$1" = "t" ] || [ "$1" = "E1-R-INT" ]; then
 		sleep 5s
 		
 	done
-
-	sed "s/CAMBIAME/${MAQUINA}/g" files/hosts > /tmp/hosts-${MAQUINA}
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
+	NOME=$(vboxmanage guestcontrol ${MAQUINA} \
+		--username administrador \
 		--password ${CONTRASINAL} \
-		copyto /tmp/hosts-${MAQUINA} /etc/hosts
+		run  --exe "/bin/hostname" |  tr -d '\r\n')
 
-	
-	sed "s/CAMBIAME/${MAQUINA}/g" files/hostname > /tmp/hostname-${MAQUINA}
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		copyto /tmp/hostname-${MAQUINA} /etc/hostname
-		
-	
-	sed "s/IPPUBLICA/${IPPUBLICA}/g" files/E1-R-INT/interfaces | \
-		sed "s/MASCARAPUBLICA/${MASCARAIPUBLICA}/g" | \
-		sed "s/GWPUBLICA/${GWPUBLICA}/g" | \
-		sed "s/IPINT/${IPINT}/g" | \
-		sed "s/MASCARAINT/${MASCARAINT}/g" \
-		> /tmp/interfaces-${MAQUINA}
-			 
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		copyto /tmp/interfaces-${MAQUINA} /etc/network/interfaces
-	
-	sed "s/DNS/${DNSINT}/g" files/E1-R-INT/resolv.conf > /tmp/resolv.conf-${MAQUINA}	
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		copyto /tmp/resolv.conf-${MAQUINA} /etc/resolv.conf
+ 	if [ "${NOME}" = "${MAQUINA}" ]; then 
+		echo A máquina xa foi configurada previamente.
+	else 
 
-
-
-	cp files/E1-R-INT/sysctl.conf /tmp
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		copyto /tmp/sysctl.conf /etc/sysctl.conf
-
-
-
-
-
-	sed "s/IPPUBLICA/${IPPUBLICA}/g" files/E1-R-INT/nftables.conf | \
-		sed "s/IPANFITRION/${IPANFITRION}/g" > /tmp/nftables.conf-${MAQUINA}	
-
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		copyto /tmp/nftables.conf-${MAQUINA} /etc/nftables.conf
-
+		sed "s/CAMBIAME/${MAQUINA}/g" files/hosts > /tmp/hosts-${MAQUINA}
+		vboxmanage guestcontrol ${MAQUINA} \
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/hosts-${MAQUINA} /etc/hosts
 
 		
+		sed "s/CAMBIAME/${MAQUINA}/g" files/hostname > /tmp/hostname-${MAQUINA}
+		vboxmanage guestcontrol ${MAQUINA} \
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/hostname-${MAQUINA} /etc/hostname
+			
+		
+		sed "s/IPPUBLICA/${IPPUBLICA}/g" files/E1-R-INT/interfaces | \
+			sed "s/MASCARAPUBLICA/${MASCARAIPUBLICA}/g" | \
+			sed "s/GWPUBLICA/${GWPUBLICA}/g" | \
+			sed "s/IPINT/${IPINT}/g" | \
+			sed "s/MASCARAINT/${MASCARAINT}/g" \
+			> /tmp/interfaces-${MAQUINA}
+				
+		vboxmanage guestcontrol ${MAQUINA} \
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/interfaces-${MAQUINA} /etc/network/interfaces
+		
+		sed "s/DNS/${DNSINT}/g" files/E1-R-INT/resolv.conf > /tmp/resolv.conf-${MAQUINA}	
+		vboxmanage guestcontrol ${MAQUINA} \
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/resolv.conf-${MAQUINA} /etc/resolv.conf
 
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		copyto $RUTAACTIVA/crontab-${MAQUINA}  /etc/crontab
-	
-	vboxmanage guestcontrol ${MAQUINA} \
-		--username ${USUARIO} \
-		--password ${CONTRASINAL} \
-		run --exe "/usr/bin/systemctl" -- enable nftables
-	
+
+
+		cp files/E1-R-INT/sysctl.conf /tmp
+		vboxmanage guestcontrol ${MAQUINA} \
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/sysctl.conf /etc/sysctl.conf
+
+
+
+
+
+		sed "s/IPPUBLICA/${IPPUBLICA}/g" files/E1-R-INT/nftables.conf | \
+			sed "s/IPANFITRION/${IPANFITRION}/g" > /tmp/nftables.conf-${MAQUINA}	
+
+		vboxmanage guestcontrol ${MAQUINA} \
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			copyto /tmp/nftables.conf-${MAQUINA} /etc/nftables.conf
+
+
+			
+		vboxmanage guestcontrol ${MAQUINA} \
+			--username ${USUARIO} \
+			--password ${CONTRASINAL} \
+			run --exe "/usr/bin/systemctl" -- enable nftables
+	fi	
 	vboxmanage controlvm ${MAQUINA} acpipowerbutton	
 
 fi
